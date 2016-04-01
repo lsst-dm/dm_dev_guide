883a09a126929a804f2d704ac5e2b3cef26570e1
Jonathan Sick <jsick@lsst.org>
2016-04-01 10:25:32 -0700

    Fix exclude path for doc snippets
    This is needed to prevent duplicate label warnings.
    
    Fixes bug introduced in DM-4793.

--- conf.py
+++ conf.py
@@ -87,7 +87,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['README.rst', '_build', 'development/docs/snippets']
+exclude_patterns = ['README.rst', '_build', 'docs/snippets']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
