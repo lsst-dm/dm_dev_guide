3ec55dd2252100c14ca7f32ee77f2080fcbe0c1e
Jonathan Sick <jsick@lsst.org>
2016-05-27 15:06:41 -0700

    Add documenteer for LSST sphinx extensions
    In particular, this enables mock code reference roles in
    documenteer.sphinxext.mockcoderefs.

--- conf.py
+++ conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx-prompt',
+    'documenteer.sphinxext'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
