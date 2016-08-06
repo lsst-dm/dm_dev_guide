40486a779f52b8a92185bb6b37b40279ff2fcfb3
Jonathan Sick <jsick@lsst.org>
2016-08-06 14:25:54 -0700

    Increase recursion limit for build
    This is a work-around for a transient build issue in Sphinx caused by
    the large and highly-nested source files we use in the developer guide.

--- conf.py
+++ conf.py
@@ -12,9 +12,12 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import sys
+import sys
 import os
 # import shlex
+
+# Work around Sphinx bug related to large and highly-nested source files
+sys.setrecursionlimit(2000)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
