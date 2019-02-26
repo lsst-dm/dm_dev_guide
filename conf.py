c4babf540930e9f79e54492cccecfae4913ee39d
Jonathan Sick <jsick@lsst.org>
2019-02-26 15:10:02 -0500

    Increment copyright to 2019

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LSST DM Developer Guide'
-copyright = u'2016-2018 Association of Universities for Research in Astronomy'
+copyright = u'2016-2019 Association of Universities for Research in Astronomy'
 author = u'LSST Data Management'
 
 # The version info for the project you're documenting, acts as replacement for
