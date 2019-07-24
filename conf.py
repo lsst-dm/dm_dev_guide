60f8d959ef7637f179653b3f008e8c72b40a89d4
Jonathan Sick <jsick@lsst.org>
2019-07-23 21:33:37 -0400

    Update to sphinx-rtd-theme 0.4.3's layout
    This merges the layout.html of the sphinx-rtd-theme at 0.4.3
    (https://raw.githubusercontent.com/readthedocs/sphinx_rtd_theme/0.4.3/sphinx_rtd_theme/layout.html)
    with the custom div to add the version switcher to the sidebar.

--- conf.py
+++ conf.py
@@ -157,7 +157,10 @@ html_context = {
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+    'style_nav_header_background': '#343131'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
