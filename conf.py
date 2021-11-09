891536073e09e920d66c68fa88c4f66c9ca323e4
Jonathan Sick <jsick@lsst.org>
2021-11-09 14:52:39 -0500

    Switch to the Rubin logo

--- conf.py
+++ conf.py
@@ -192,7 +192,7 @@ html_baseurl = "https://developer.lsst.io/"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/lsst-logo-dark.svg'
+html_logo = '_static/rubin-imagotype-color-on-black.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
